52c0354749c5079eed7cda69c681f576beab322b
Skyonave <Skyonave@gmail.com>
2022-10-26 01:47:21 +0700

    fix

--- js/slider.js
+++ js/slider.js
@@ -3,20 +3,7 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 1.2,
-            spaceBetween: -10
-        },
-        480: {
-            slidesPerView: 2,
-            spaceBetween: 0
-        },
-        640: {
-            slidesPerView: 3,
-            spaceBetween: 10
-        }
-    }
+    slidesPerView: 1.2,
 });
 
 let cardContentWrapper = document.querySelector(".brand-list__wrapper");
@@ -25,11 +12,11 @@ let showNext = document.querySelector('.show-button');
 
 showNext.onclick = function () {
     if(showNext.textContent === "Показать все") {
-        cardContentWrapper.style.height = "365px";
+        cardContentWrapper.style.maxHeight = "11350px"; 
         showNext.textContent = "Скрыть";
         showNext.classList.add('reverse');
     } else {
-        cardContentWrapper.style.height = "175px";
+        cardContentWrapper.style.maxHeight = "175px";
         showNext.textContent = "Показать все";
         showNext.classList.remove('reverse');
     }
